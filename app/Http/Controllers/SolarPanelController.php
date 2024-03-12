ecf85f3134e8f1d1a2bd9fb112c6ed2da6d66410
RoanKnight <N00222593@iadt.ie>
2024-03-12 12:51:20 +0000

    small update

--- app/Http/Controllers/SolarPanelController.php
+++ app/Http/Controllers/SolarPanelController.php
@@ -37,6 +37,9 @@ class SolarPanelController extends Controller
     } else {
       // If the date entry exists, add the next hour entry only if the last hour is not '23:00'
       $lastHourEntry = end($dateEntry['hours']);
+      if ($lastHourEntry['hour'] === '23:00') {
+        return response()->json('Cannot add any more objects for the day', 200);
+      }
       $nextHour = $lastHourEntry ? ((int)substr($lastHourEntry['hour'], 0, 2) + 1) % 24 : 0;
       $dateEntry['hours'][] = [
         'hour' => sprintf('%02d:00', $nextHour),
